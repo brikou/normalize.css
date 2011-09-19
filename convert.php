f232d422f3fca0c54fc2d7b4acbcf759fd1c0d97
Brikou CARRE <brikou@gmail.com>
2011-09-19 09:17:47 +0200

    added convert script to keep normalize up to date

---
+++ convert.php
@@ -0,0 +1,17 @@
+#!/usr/bin/env php
+<?php
+
+$f = '_normalize.scss';
+
+chdir(__DIR__);
+
+shell_exec('sass-convert normalize.css '.$f);
+
+$scss = file_get_contents($f);
+$scss = preg_replace('%/\*.*?\*/%s', '', $scss);
+$scss = preg_replace('/^[ \t]*$\n/m', '', $scss);
+$scss = preg_replace('/;[ \t]*\n^[ \t]*\}$/m', '; }', $scss);
+$scss = preg_replace('/^(\s*)/m', '$1$1', $scss);
+$scss = preg_replace('/\}$\n^([a-z])/m', "}\n\n\$1", $scss);
+
+file_put_contents($f, $scss);
