127389317cc79cdb5631925982ce2fb71cf9fd9a
Brikou CARRE <brikou@gmail.com>
2011-10-28 08:16:52 +0200

    update normalize.css version and version is now displayed in scss file

--- convert.php
+++ convert.php
@@ -8,7 +8,7 @@ chdir(__DIR__);
 shell_exec('sass-convert normalize.css '.$f);
 
 $scss = file_get_contents($f);
-$scss = preg_replace('%/\*.*?\*/%s', '', $scss);
+$scss = preg_replace('%/\*[^!].*?\*/%s', '', $scss);
 $scss = preg_replace('/^[ \t]*$\n/m', '', $scss);
 $scss = preg_replace('/;[ \t]*\n^[ \t]*\}$/m', '; }', $scss);
 $scss = preg_replace('/^(\s*)/m', '$1$1', $scss);
